d44d482a1ba1d892af131c2abc8c35857f6f5e7a
Matthew Oh <m4tthew.0h@gmail.com>
2024-05-23 18:33:37 -0700

    Hour 20

---
+++ moh_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,11 @@
+/*
+Questions
+1. The game feels too easy. The obstacles are quite easy to avoid because of their small size and lots of time to react due to the speed.
+2. Make the obstacles faster or larger or more in quantity.
+3. Make the animation smooth when transitioning in and out of the slowdown and differnet kind of obstacles
+4. Getting powerups and highscores are fun.
+5. Being unable to get a powerup because it spawns too far away and the repetititveness of the game are tedious.
+
+Changes
+Added new particles to both pickups
+*/
